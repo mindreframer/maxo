4a3b1dadb6573527d76d47ad8633a59d2292462f
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-05 12:50:45 +0200

    Installer

---
+++ installer/lib/maxo_new.ex
@@ -0,0 +1,18 @@
+defmodule MaxoNew do
+  @moduledoc """
+  Documentation for `MaxoNew`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MaxoNew.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ installer/test/maxo_ne_test.exs
@@ -0,0 +1,8 @@
+defmodule MaxoNewTest do
+  use ExUnit.Case
+  doctest MaxoNew
+
+  test "greets the world" do
+    assert MaxoNew.hello() == :world
+  end
+end
