cc47e28f47441df1c521dd6cdbbe2db0c46a00d1
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-06 22:56:50 +0200

    Feat: live tests with URL helpers

--- priv/templates/maxo.gen.live/live_test.exs
+++ priv/templates/maxo.gen.live/live_test.exs
@@ -17,19 +17,19 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
     setup [:create_<%= schema.singular %>]
 
     test "lists all <%= schema.plural %>", <%= if schema.string_attr do %>%{conn: conn, <%= schema.singular %>: <%= schema.singular %>}<% else %>%{conn: conn}<% end %> do
-      {:ok, _index_live, html} = live(conn, ~p"<%= schema.route_prefix %>")
+      {:ok, _index_live, html} = live(conn, index_path())
 
       assert html =~ "Listing <%= schema.human_plural %>"<%= if schema.string_attr do %>
       assert html =~ <%= schema.singular %>.<%= schema.string_attr %><% end %>
     end
 
     test "saves new <%= schema.singular %>", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, ~p"<%= schema.route_prefix %>")
+      {:ok, index_live, _html} = live(conn, index_path())
 
       assert index_live |> element("a", "New <%= schema.human_singular %>") |> render_click() =~
                "New <%= schema.human_singular %>"
 
-      assert_patch(index_live, ~p"<%= schema.route_prefix %>/new")
+      assert_patch(index_live, new_path())
 
       assert index_live
              |> form("#<%= schema.singular %>-form", <%= schema.singular %>: @invalid_attrs)
@@ -39,7 +39,7 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
              |> form("#<%= schema.singular %>-form", <%= schema.singular %>: @create_attrs)
              |> render_submit()
 
-      assert_patch(index_live, ~p"<%= schema.route_prefix %>")
+      assert_patch(index_live, index_path())
 
       html = render(index_live)
       assert html =~ "<%= schema.human_singular %> created successfully"<%= if schema.string_attr do %>
@@ -47,12 +47,12 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
     end
 
     test "updates <%= schema.singular %> in listing", %{conn: conn, <%= schema.singular %>: <%= schema.singular %>} do
-      {:ok, index_live, _html} = live(conn, ~p"<%= schema.route_prefix %>")
+      {:ok, index_live, _html} = live(conn, index_path())
 
       assert index_live |> element("#<%= schema.plural %>-#{<%= schema.singular %>.id} a", "Edit") |> render_click() =~
                "Edit <%= schema.human_singular %>"
 
-      assert_patch(index_live, ~p"<%= schema.route_prefix %>/#{<%= schema.singular %>}/edit")
+      assert_patch(index_live, index_edit_path(<%= schema.singular %>))
 
       assert index_live
              |> form("#<%= schema.singular %>-form", <%= schema.singular %>: @invalid_attrs)
@@ -62,7 +62,7 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
              |> form("#<%= schema.singular %>-form", <%= schema.singular %>: @update_attrs)
              |> render_submit()
 
-      assert_patch(index_live, ~p"<%= schema.route_prefix %>")
+      assert_patch(index_live, index_path())
 
       html = render(index_live)
       assert html =~ "<%= schema.human_singular %> updated successfully"<%= if schema.string_attr do %>
@@ -70,7 +70,7 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
     end
 
     test "deletes <%= schema.singular %> in listing", %{conn: conn, <%= schema.singular %>: <%= schema.singular %>} do
-      {:ok, index_live, _html} = live(conn, ~p"<%= schema.route_prefix %>")
+      {:ok, index_live, _html} = live(conn, index_path())
 
       assert index_live |> element("#<%= schema.plural %>-#{<%= schema.singular %>.id} a", "Delete") |> render_click()
       refute has_element?(index_live, "#<%= schema.plural %>-#{<%= schema.singular %>.id}")
@@ -81,19 +81,19 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
     setup [:create_<%= schema.singular %>]
 
     test "displays <%= schema.singular %>", %{conn: conn, <%= schema.singular %>: <%= schema.singular %>} do
-      {:ok, _show_live, html} = live(conn, ~p"<%= schema.route_prefix %>/#{<%= schema.singular %>}")
+      {:ok, _show_live, html} = live(conn, show_path(<%= schema.singular %>))
 
       assert html =~ "Show <%= schema.human_singular %>"<%= if schema.string_attr do %>
       assert html =~ <%= schema.singular %>.<%= schema.string_attr %><% end %>
     end
 
     test "updates <%= schema.singular %> within modal", %{conn: conn, <%= schema.singular %>: <%= schema.singular %>} do
-      {:ok, show_live, _html} = live(conn, ~p"<%= schema.route_prefix %>/#{<%= schema.singular %>}")
+      {:ok, show_live, _html} = live(conn, show_path(<%= schema.singular %>))
 
       assert show_live |> element("a", "Edit") |> render_click() =~
                "Edit <%= schema.human_singular %>"
 
-      assert_patch(show_live, ~p"<%= schema.route_prefix %>/#{<%= schema.singular %>}/show/edit")
+      assert_patch(show_live, show_edit_path(<%= schema.singular %>))
 
       assert show_live
              |> form("#<%= schema.singular %>-form", <%= schema.singular %>: @invalid_attrs)
@@ -103,11 +103,18 @@ defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web
              |> form("#<%= schema.singular %>-form", <%= schema.singular %>: @update_attrs)
              |> render_submit()
 
-      assert_patch(show_live, ~p"<%= schema.route_prefix %>/#{<%= schema.singular %>}")
+      assert_patch(show_live, show_path(<%= schema.singular %>))
 
       html = render(show_live)
       assert html =~ "<%= schema.human_singular %> updated successfully"<%= if schema.string_attr do %>
       assert html =~ "some updated <%= schema.string_attr %>"<% end %>
     end
+
+    # URL utils
+    defp index_path, do: ~p"<%= schema.route_prefix %>"
+    defp new_path, do: ~p"<%= schema.route_prefix %>/new"
+    defp index_edit_path(<%= schema.singular %>), do: ~p"<%= schema.route_prefix %>/#{<%= schema.singular %>}/edit"
+    defp show_path(<%= schema.singular %>), do: ~p"<%= schema.route_prefix %>/#{<%= schema.singular %>}"
+    defp show_edit_path(<%= schema.singular %>), do: ~p"<%= schema.route_prefix %>/#{<%= schema.singular %>}/show/edit"
   end
 end
