ae2bd4d536867e7ce4ab2a3a93117a5ba4ae699e
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-09 21:28:13 +0200

    CI: disable force update for mneme

--- installer/lib/test_helper.exs
+++ installer/lib/test_helper.exs
@@ -4,7 +4,7 @@ ExUnit.start(trace: true)
 defmodule MnemeDefauls do
   defmacro __using__(_) do
     quote do
-      use Mneme, action: :accept, default_pattern: :last, force_update: true
+      use Mneme, action: :accept, default_pattern: :last, force_update: false
     end
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,7 +3,7 @@ ExUnit.start()
 defmodule MnemeDefauls do
   defmacro __using__(_) do
     quote do
-      use Mneme, action: :accept, default_pattern: :last, force_update: true
+      use Mneme, action: :accept, default_pattern: :last, force_update: false
     end
   end
 end
